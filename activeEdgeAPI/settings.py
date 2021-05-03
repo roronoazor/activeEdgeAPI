6049efdfd27bb6729b07b0bc78dc554737bfec39
roronoazor <ugoodumegwu@gmail.com>
2021-05-03 04:17:45 +0000

    updated settings.py

--- activeEdgeAPI/settings.py
+++ activeEdgeAPI/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-wh5$@3@nn^5m!a4y*=($jq9no2r82^-y0q)a&n=oz64p$21iva
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["activeedgeapi.herokuapp.com"]
 
 
 # Application definition
@@ -77,12 +78,19 @@ WSGI_APPLICATION = 'activeEdgeAPI.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+db = "prod"
+
+if db == "prod":
+    DATABASES = dict()
+    prod_db = dj_database_url.config(conn_max_age=500)
+    DATABASES['default'].update(prod_db)
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
 
 
 # Password validation
